d301528caf5d993f2e4159d0e04b1e57a35ec276
raysartm <89471576+raysartm@users.noreply.github.com>
2024-07-16 12:23:15 +0530

    Update Video_Processor.py

--- Video_Processor.py
+++ Video_Processor.py
@@ -10,7 +10,7 @@ def set_bg_image(image_file):
     <style>
     [data-testid="stAppViewContainer"] {{
         background: url("data:image/png;base64,{b64}") no-repeat center center fixed;
-        background-size: cover;
+        background-size: 200%;
     }}
     h1, h2, h3, h4, h5, h6, p {{
         color: black;
