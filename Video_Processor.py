0defad79095558e3d06dbd33411fe7acaabccb93
raysartm <89471576+raysartm@users.noreply.github.com>
2024-07-16 11:50:35 +0530

    Update Video_Processor.py

--- Video_Processor.py
+++ Video_Processor.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
-# Define the CSS for the gradient background
+# Define the CSS for the image background and white text
 page_bg_img = '''
 <style>
 [data-testid="stAppViewContainer"] {
-    background: linear-gradient(to right, #00FFFF, #008080);
-    background-attachment: fixed;
+    background: url("https://images.pexels.com/photos/5603660/pexels-photo-5603660.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1") no-repeat center center fixed;
+    background-size: cover;
+}
+[data-testid="stMarkdownContainer"] h1, h2, h3, h4, h5, h6, p {
+    color: white;
 }
 </style>
 '''
@@ -31,7 +34,7 @@ st.markdown("### 3. Automatic Number Plate Detection")
 st.write("Identify and extract vehicle number plates from videos, useful for security and traffic monitoring.")
 
 st.markdown("### 4. Transcribe Video ")
-st.write("Convert the audio from your video into text for at the click of a button.")
+st.write("Convert the audio from your video into text with a click of a button.")
 
 st.markdown("### 5. Change Video Speed")
 st.write("Adjust the playback speed of your video to either slow down or speed up the content as needed.")
