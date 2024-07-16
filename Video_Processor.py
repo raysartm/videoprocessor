3f37622dd843ec904d323d1239a3f1b725767a20
raysartm <89471576+raysartm@users.noreply.github.com>
2024-07-16 11:41:27 +0530

    Update Video_Processor.py

--- Video_Processor.py
+++ Video_Processor.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Define the CSS for the gradient background
 page_bg_img = '''
 <style>
-body {
+[data-testid="stAppViewContainer"] {
     background: linear-gradient(to right, #00FFFF, #008080);
     background-attachment: fixed;
 }
@@ -15,8 +15,10 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.title("Video Processor App")
 st.write("""
-Welcome to the Video Processor App. This app allows you to perform various video processing tasks.
-You can choose from the following functionalities:
+Welcome to VideoPro! 
+Here's an app to perform your various video processing needs at one place.
+
+You can choose from any of the following functionalities:
 """)
 
 st.markdown("### 1. Object Detection")
@@ -28,8 +30,8 @@ st.write("Generate a concise summary of your video, highlighting the most import
 st.markdown("### 3. Automatic Number Plate Detection")
 st.write("Identify and extract vehicle number plates from videos, useful for security and traffic monitoring.")
 
-st.markdown("### 4. Transcribe Video using IBM Watson")
-st.write("Convert the audio from your video into text using IBM Watson's advanced speech recognition technology.")
+st.markdown("### 4. Transcribe Video ")
+st.write("Convert the audio from your video into text for at the click of a button.")
 
 st.markdown("### 5. Change Video Speed")
 st.write("Adjust the playback speed of your video to either slow down or speed up the content as needed.")
